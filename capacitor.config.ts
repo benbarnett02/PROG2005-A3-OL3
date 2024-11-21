dc09d885485f75c261bb4e98e6f5d4cf3556710a
benbarnett02 <benjamin.barnett02@gmail.com>
2024-11-21 17:33:18 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROG2005-A3-OL3',
+  webDir: 'www'
+};
+
+export default config;
