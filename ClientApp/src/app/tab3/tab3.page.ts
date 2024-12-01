9e3c3fb7615853454c65f5b47c2a7cbb0b760602
benbarnett02 <benjamin.barnett02@gmail.com>
2024-12-01 12:09:03 +1100

    Created workouts tab

--- ClientApp/src/app/tab3/tab3.page.ts
+++ ClientApp/src/app/tab3/tab3.page.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClientService, Client, Workout} from '../services/data.service';
+import {AuthService} from "../services/auth.service";
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  private client: Client;
+  private workoutPlan: Workout[] | undefined;
+  groupedWorkouts: { day: string; workouts: Workout[] }[] = [];
+
+
+  constructor(private dataService: ClientService, private authService: AuthService) {
+    this.client = this.authService.getCurrentClient() as Client; // explicit cast... not ideal but in theory this should always be a Client if router guard hasn't redirected.
+    this.dataService.getWorkoutPlan(this.client.client_id).subscribe((workoutPlan) => {
+      this.workoutPlan = workoutPlan;
+      this.groupWorkoutsByDay(); // this method runs twice - guessing it fixes the race condition?
+
+    });
+
+  }
+
+  ngOnInit() {
+    this.groupWorkoutsByDay();
+  }
+
+  groupWorkoutsByDay() {
+    if (!this.workoutPlan) {
+      return;
+    }
+    this.groupedWorkouts = this.workoutPlan.reduce((acc, workout) => {
+      const day = workout.day;
+      const existingDay = acc.find((group) => group.day === day);
+      if (existingDay) {
+        existingDay.workouts.push(workout);
+      } else {
+        acc.push({day, workouts: [workout]});
+      }
+      return acc;
+    }, [] as { day: string; workouts: Workout[] }[]);
+  }
 
 }
