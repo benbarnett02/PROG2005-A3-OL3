5045d3593e34dbcb30e8f903f13a87b01d733879
KingMoses252 <95511496+KingMoses252@users.noreply.github.com>
2024-12-09 23:12:56 +1000

    Small Update

--- TrainerApp/src/app/pages/home/home.page.ts
+++ TrainerApp/src/app/pages/home/home.page.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
   authors = [
-    'Musarrat Chowdhury',
-    'Assistant Developer'
+    'Musa Mohammed',
+    'Ben'
   ];
 
   constructor() { }
